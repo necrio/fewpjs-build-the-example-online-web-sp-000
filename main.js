de49162f932168af1bc1177f3bb09be1e60e9ba9
Keith Isham <kisham8899@gmail.com>
2020-07-09 18:02:48 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,59 +3,7 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
 
-let glyphStates = {
-  "♡": "♥",
-  "♥": "♡"
-};
-
-let colorStates = {
-  "red" : "",
-  "": "red"
-};
-
-let articleHearts = document.querySelectorAll(".like");
-
-function likeCallback(e) {
-  let heart = e.target;
-  mimicServerCall("bogusUrl")
-   //OR: mimicServerCall("bogusUrl", {forceFailure: true})
-    .then(function(serverMessage){
-       heart.innerText = glyphStates[heart.innerText];
-       heart.style.color = colorStates[heart.style.color];
-    })
-    .catch(function(error) {
-      // Basic
-      // alert("Something went wrong!");
-      // or....
-      document.getElementById("modal").className = "";
-    });
-}
-
-for (let glyph of articleHearts) {
-  glyph.addEventListener("click", likeCallback);
-}
-
-
-//------------------------------------------------------------------------------
-// Ignore after this point. Used only for demo purposes
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
-}
 
 
 
